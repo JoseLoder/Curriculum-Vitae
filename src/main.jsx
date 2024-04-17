6f1920165319356195cb269787a440e107cfd317
MartinSilvaJose <118887247+MartinSilvaJose@users.noreply.github.com>
2024-04-17 12:02:50 +0200

    Add Material-UI dependencies and implement theme in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+/* Material-UI */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+export const themeOptions = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#2196f3',
+    },
+    secondary: {
+      main: '#ff5722',
+    },
+  },
+});
+
+
+/* React Router */
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import './index.css';
 
 import ErrorPage from './ErrorPage';
 import { LandingPage } from './pages/LandingPage';
@@ -31,6 +53,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={themeOptions}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
