494efad4610694529a4ee554a19e9085e1f0e10e
MartinSilvaJose <118887247+MartinSilvaJose@users.noreply.github.com>
2024-04-20 22:38:47 +0200

    error

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,22 @@
+import { useRouteError, useNavigate } from "react-router-dom";
+
+export default function ErrorPage() {
+    const error = useRouteError();
+    const navigate = useNavigate();
+    console.error(error);
+
+    const redirectToLandingPage = () => {
+        navigate('/landingpage');
+    }
+
+    return (
+        <div id="error-page">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+            <button onClick={redirectToLandingPage}>Go to Landing Page</button>
+        </div>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -28,6 +28,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import ErrorPage from './ErrorPage';
 import { LandingPage } from './pages/LandingPage';
 import { Proyects } from './pages/Proyects';
 import { Experience } from './pages/Experience';
@@ -36,14 +37,17 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/proyects",
     element: <Proyects />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/experience",
     element: <Experience />,
+    errorElement: <ErrorPage />,
   }
 ]);
 
