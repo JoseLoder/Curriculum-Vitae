fa04e7bfd6ab8b0c0d8b3de4483130235156a62d
José Martín <martin_silva_jose@hotmail.com>
2024-09-02 13:20:03 +0200

    Update Proyects.jsx

--- src/pages/Proyects.jsx
+++ src/pages/Proyects.jsx
@@ -42,7 +42,7 @@ export function Proyects() {
                         thirdImage='/elefante-azul_3.png'
                         title="Elefante Azul - Laravel"
                         description="Un proyecto realizado con Laravel, es un sitio web para pedir cita devolviendo un ticket con la hora y datos relacionados. Además si eres administrador podrás ver todas las citas."
-                        code="https://github.com/MartinSilvaJose/Elefante-Azul.git"
+                        code="https://github.com/joseloder/Elefante-Azul.git"
 
                     />
                     <CardProyect
@@ -51,7 +51,7 @@ export function Proyects() {
                         thirdImage='/client-curl_3.png'
                         title="Cliente cURL - PHP"
                         description="Un proyecto realizado con PHP, que se conecta a la API REST del Elefante Azul está hecho con php y cURL. Esta API REST administra todo lo necesario para pedir las citas e imprimir los tickets."
-                        code="https://github.com/MartinSilvaJose/Cliente-API-REST-Curl.git"
+                        code="https://github.com/joseloder/Cliente-API-REST-Curl.git"
                     />
                     <CardProyect
                         firstImage='/reservation-form_1.png'
@@ -59,8 +59,8 @@ export function Proyects() {
                         thirdImage='/reservation-form_3.png'
                         title="Reservation Form - JS Vanilla"
                         description="Un proyecto realizado con HTML, CSS y JavaScript, que simula un formulario de reserva de un hotel. Este formulario tiene validaciones y un botón de enviar que muestra un mensaje de éxito."
-                        code="https://github.com/MartinSilvaJose/Reservation-Form.git"
-                        demo="https://martinsilvajose.github.io/Reservation-Form/"
+                        code="https://github.com/joseloder/Reservation-Form.git"
+                        demo="https://joseloder.github.io/Reservation-Form/"
                     />
                     <CardProyect
                         firstImage='/working-the-dom_1.png'
@@ -68,8 +68,8 @@ export function Proyects() {
                         thirdImage='/working-the-dom_3.png'
                         title="Working the DOM - JS Vanilla"
                         description="Un proyecto realizado con HTML, CSS y JavaScript con funcionalidad CRUD utilizando el DOM. Cuando modificas puedes alterar la posición del registro con las flechas."
-                        code="https://github.com/MartinSilvaJose/Working-The-DOM.git"
-                        demo="https://martinsilvajose.github.io/Working-The-DOM/"
+                        code="https://github.com/joseloder/Working-The-DOM.git"
+                        demo="https://joseloder.github.io/Working-The-DOM/"
                     />
                     <CardProyect
                         firstImage='/connect-4_1.png'
@@ -77,8 +77,8 @@ export function Proyects() {
                         thirdImage='/connect-4_3.png'
                         title="Connect 4 - React"
                         description="Un proyecto realizado con React y JavaScript. Este juego es el clásico 4 en raya, donde puedes jugar contra un amigo, fué mi primer proyecto personal realizado con React."
-                        code="https://github.com/MartinSilvaJose/Connect-4.git"
-                        demo="https://martinsilvajose.github.io/Connect-4/"
+                        code="https://github.com/joseloder/Connect-4.git"
+                        demo="https://joseloder.github.io/Connect-4/"
                     />
                     <CardProyect
                         firstImage='/api-cat-and-dog_1.png'
@@ -86,8 +86,8 @@ export function Proyects() {
                         thirdImage='/api-cat-and-dog_3.png'
                         title="Api Cats and Dogs - React"
                         description="Un proyecto realizado con React, JavaScript y CSS, es una aplicación que consume una API de gatos y perros. Puedes ver las fotos de los gatos y perros y cambiar entre ellos."
-                        code="https://github.com/MartinSilvaJose/Api-Cat-and-Dog.git"
-                        demo="https://martinsilvajose.github.io/Api-Cat-and-Dog/"
+                        code="https://github.com/joseloder/Api-Cat-and-Dog.git"
+                        demo="https://joseloder.github.io/Api-Cat-and-Dog/"
                     />
                     <CardProyect
                         firstImage='/api-game-of-thrones_1.png'
@@ -95,8 +95,8 @@ export function Proyects() {
                         thirdImage='/api-game-of-thrones_3.png'
                         title="Api Game of Thrones - React"
                         description="Un proyecto realizado con React y consume API de Game of Thrones. Puedes seleccionar los personajes mas influyentes de la serie y sus freses más icónicas."
-                        code="https://github.com/MartinSilvaJose/Api-Game-of-Thrones.git"
-                        demo="https://martinsilvajose.github.io/Api-Game-of-Thrones/"
+                        code="https://github.com/joseloder/Api-Game-of-Thrones.git"
+                        demo="https://joseloder.github.io/Api-Game-of-Thrones/"
                     />
                     <CardProyect
                         firstImage='/api-detect-lenguage_1.png'
@@ -104,8 +104,8 @@ export function Proyects() {
                         thirdImage='/api-detect-lenguage_3.png'
                         title="Api Detect Lenguage - React"
                         description="Un proyecto realizado con React y consume API un tanto compleja ya que requiere de instalación previa. Consiste en detectar el idioma del texto introducido."
-                        code="https://github.com/MartinSilvaJose/Api-Detect-Lenguage.git"
-                        demo="https://martinsilvajose.github.io/Api-Detect-Lenguage/"
+                        code="https://github.com/joseloder/Api-Detect-Lenguage.git"
+                        demo="https://joseloder.github.io/Api-Detect-Lenguage/"
                     />
                     <CardProyect
                         firstImage='/api-pokemon_1.png'
@@ -113,8 +113,8 @@ export function Proyects() {
                         thirdImage='/api-pokemon_3.png'
                         title="Api Pokémon - React"
                         description="Un proyecto realizado con React el cual consume la pokeAPI, en este caso nos irá mostrando los pokemons de 30 en 30 con la funcionalidad de scroll infinito."
-                        code="https://github.com/MartinSilvaJose/Api-Pokemon.git"
-                        demo="https://martinsilvajose.github.io/Api-Pokemon/"
+                        code="https://github.com/joseloder/Api-Pokemon.git"
+                        demo="https://joseloder.github.io/Api-Pokemon/"
                     />
                     <CardProyect
                         firstImage='/crud-javafx_1.png'
@@ -122,7 +122,7 @@ export function Proyects() {
                         thirdImage='/crud-javafx_3.png'
                         title="CRUD JavaFX - Java"
                         description="Este proyecto fué realizado para impartir una clase de adaptación de un proyecto Java basado en MVC donde la vista es realizada con JavaFX y es totalmente independiente del modelo."
-                        code="https://github.com/MartinSilvaJose/CRUDJavaFX-Terminado"
+                        code="https://github.com/joseloder/CRUDJavaFX-Terminado"
                     />
                 </Box>
 
